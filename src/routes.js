47bb89330cd2b3e44c212d72039c935216162c39
Jéssica Rosália <jessicarosalia678@gmail.com>
2021-08-24 10:30:13 -0300

    Retirando rotas inutilizadas no momento

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import 'react-native-gesture-handler';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Text } from 'react-native';
 
 const Stack = createStackNavigator();
 
 const Routes = () => {
 
     return (
-        <Stack.Navigator>
-            <Stack.Screen name="Login" component={Login} />
-        </Stack.Navigator>
+        <Text>.</Text>
     )
 }
 
