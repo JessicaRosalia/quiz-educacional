84578f7f243bb075c880441ef5078972a49730f2
Jéssica Rosália <jessicarosalia678@gmail.com>
2021-08-24 10:46:59 -0300

    Removendo react native navigation

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import 'react-native-gesture-handler';
-import { createStackNavigator } from '@react-navigation/stack';
-//import { NavigationContainer } from '@react-navigation/native';
+//import 'react-native-gesture-handler';
 import { Text } from 'react-native';
-
-const Stack = createStackNavigator();
 
 
 const Routes = () => {
