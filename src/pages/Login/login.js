fbd7b489ea6723b5bd5a00c6341d4b14bce95719
dimonalaquis <dimona.laquis@hotmail.com>
2021-08-30 11:55:52 -0300

    Inseri o nome do botão de entrar

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Field from '../../components/Field/index';
 import TabNav from '../../components/TabNav/index';
@@ -13,6 +13,9 @@ const Login = () => {
                 <TabNav login={true} register={false} page="login" />
                 <Field label="E-mail" placeholder="exemplo@gmail.com" keyboardType="email-address"/>
                 <Field label="Senha" placeholder="Sua senha" keyboardType="default" />
+                <TouchableOpacity>
+                    <Text>Entrar</Text>
+                </TouchableOpacity>
             </View>
         </SafeAreaView>
     )
