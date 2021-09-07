91323c799845a31c00f41c2361e781bc5821c72c
dimonalaquis <dimona.laquis@hotmail.com>
2021-09-07 16:53:10 -0300

    implemetcao do button to enter

---
+++ src/pages/Login/style.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+
+export default StyleSheet.create({
+    containerButton:{
+               
+        backgroundColor:'#0C066B',
+        width: 200,
+        height: 54,
+        borderRadius: 10,
+        borderWidth:5,
+        borderColor: '#fff',
+        justifyContent: 'center',
+    },
+    enterText:{
+       color:'#fff',
+       textAlign:"center",
+       justifyContent:'center',
+       fontSize: 16,
+    }
+});
+   
