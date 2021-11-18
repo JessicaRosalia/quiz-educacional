6b797d88c08c6f0f08b58b278ec00a3de5c96277
Lucas Lima <lucasflima25@gmail.com>
2021-11-18 20:22:40 -0300

    Comentario sobre rotas

--- back/src/api/routes/routes.ts
+++ back/src/api/routes/routes.ts
@@ -14,6 +14,7 @@ routes.get("/", (req: express.Request, res: express.Response) => {
     res.json({ "msg": "Alive!!" });
 });
 
+// All routes after here needs to have a valid token to run
 routes.use(verifyToken)
 
 routes.use("/user", userRouter);
