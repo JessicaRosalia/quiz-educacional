93968ecd2075bb407c32c5ed42ac17313fa7768b
Jéssica Rosália <jessicarosalia678@gmail.com>
2021-12-10 20:11:22 -0300

    Montada estrutura inicial da tela de perguntas

--- front/src/pages/QuestionScreen/QuestionScreen.js
+++ front/src/pages/QuestionScreen/QuestionScreen.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
+import QuestionAnswerField from '../../components/QuestionAnswerField';
+import style from '../QuestionScreen/style';
 
 function QuestionScreen() {
     return (
-        
-        <Text>Teste</Text>
+        <View>
+            <View style={style.questionScreenHeader}>
+                <Text>Nome 1</Text>
+                <View style={style.infoMatch}>
+                    <Text>Tempo Aqui</Text>
+                    <Text>Área</Text>
+                </View>
+                <Text>Nome 2</Text>
+                
+            </View>
+            <View style={style.answerQuestion}>
+                <Text>PERGUNTA AQUI</Text>
+            </View>
+            <View>
+                <QuestionAnswerField answerText="Resposta1"/>
+                <QuestionAnswerField answerText="Resposta2"/>
+                <QuestionAnswerField answerText="Resposta3"/>
+                <QuestionAnswerField answerText="Resposta4"/>
+            </View>
+        </View>
         
     )
 }
---
+++ front/src/pages/QuestionScreen/style.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    answerQuestion: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    questionScreenHeader: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        flexDirection: "row",
+    },
+    infoMatch: {
+        display: "flex",
+        alignItems: "center",
+    }
+
+})
