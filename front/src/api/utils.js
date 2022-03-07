a5716ebca71f9b4cafedc8e90aba5a55dd684c9c
Jessica Rosalia Fernandes <jessicarosalia678@gmail.com>
2022-03-07 08:05:12 -0300

    Deixando o cadastro dinâmico e passando tudo pra inglês

--- front/src/api/utils.js
+++ front/src/api/utils.js
@@ -1,7 +1,19 @@
-import base64 from 'react-native-base64'
+import React from 'react';
+import base64 from 'react-native-base64';
+import createAxiosInstance from './';
+
+
 
 export function getUserInfo(token) {
     const infosrc = base64.decode(token.split(".")[1]).replace(/\0/g, '')
     const info = JSON.parse(infosrc);
     return info.user;
+}
+
+export async function postQuestion(parameters){
+
+    const axios = await createAxiosInstance();
+    const response = await axios.post('/question', parameters, {
+    });
+    return response.data;
 }
--- front/src/pages/QuestionRegistration/QuestionRegistration.js
+++ front/src/pages/QuestionRegistration/QuestionRegistration.js
@@ -1,41 +1,67 @@
 import React, { useEffect, useState } from 'react';
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
 import InputText from '../../components/InputText';
 import Label from '../../components/Label';
 import Select from '../../components/Select';
 import { RadioButton } from 'react-native-paper';
 import { ScrollView } from 'react-native-gesture-handler';
+import { postQuestion } from '../../api/utils';
+import { Button } from 'react-native-elements/dist/buttons/Button';
+import { TouchableHighlight } from 'react-native';
 
 const QuestionRegistration = () => {
 
-    const [descricaoPergunta, setDescricaoPergunta] = useState("");
-    const [alternativaA, setAlternativaA] = useState("");
-    const [alternativaB, setAlternativaB] = useState("");
-    const [alternativaC, setAlternativaC] = useState("");
-    const [alternativaD, setAlternativaD] = useState("");
+    const [questionDescription, setQuestionDescription] = useState("");
+    const [alternativaA, setAlternativeA] = useState("");
+    const [alternativeB, setAlternativeB] = useState("");
+    const [alternativeC, setAlternativeC] = useState("");
+    const [alternativeD, setAlternativeD] = useState("");
     const [selectedValue, setSelectedValue] = useState("Selecione");
+    const [question, setQuestion] = useState();
 
-
-    const handlerSelectedValue = (itemValue) => {
-        setSelectedValue(itemValue);
+    const questionParam = {
+        prompt: questionDescription,
+        options: [
+            {
+                body: alternativaA,
+                answer: false
+            },
+            {
+                body: alternativeB,
+                answer: false
+            },
+            {
+                body: alternativeC,
+                answer: true
+            },
+            {
+                body: alternativeD,
+                answer: false
+            }
+        ]
     }
 
+    async function registerQuestion () {
+        await postQuestion(questionParam).then(question=>{
+            setQuestion(question);
+        }).catch(()=>{
+            console.log("Ocorreu um erro ao tentar cadastrar a questão. Você pode tentar novamente.");
+        })
+    }
 
-    useEffect(()=> {
-        console.log(selectedValue);
-    })
 
     return (
         <View>
             <ScrollView>
                 <Label label={"Matéria"} required={true}/>
                 <Select onChangeValueSelected={setSelectedValue} selectedValue={selectedValue}/>
-                <InputText label={"Pergunta"} required={true} placeholder={"Informe o enunciado da pergunta"} onChangeValue={setDescricaoPergunta} />
+                <InputText label={"Pergunta"} required={true} placeholder={"Informe o enunciado da pergunta"} onChangeValue={setQuestionDescription} />
                 <Label label={"Alternativas de resposta"} required={false}/>
-                <InputText label={"Alternativa A"} placeholder={"Informe o texto da alternativa A"} onChangeValue={setAlternativaA} />
-                <InputText label={"Alternativa B"} placeholder={"Informe o texto da alternativa B"} onChangeValue={setAlternativaB}/>
-                <InputText label={"Alternativa C"} placeholder={"Informe o texto da alternativa C"} onChangeValue={setAlternativaC}/>
-                <InputText label={"Alternativa D"} placeholder={"Informe o texto da alternativa D"} onChangeValue={setAlternativaD}/>
+                <InputText label={"Alternativa A"} placeholder={"Informe o texto da alternativa A"} onChangeValue={setAlternativeA} />
+                <InputText label={"Alternativa B"} placeholder={"Informe o texto da alternativa B"} onChangeValue={setAlternativeB}/>
+                <InputText label={"Alternativa C"} placeholder={"Informe o texto da alternativa C"} onChangeValue={setAlternativeC}/>
+                <InputText label={"Alternativa D"} placeholder={"Informe o texto da alternativa D"} onChangeValue={setAlternativeD}/>
+                <TouchableHighlight style={{backgroundColor: "#ccc"}} onPress={registerQuestion}><Text>Cadastrar</Text></TouchableHighlight>
             </ScrollView>
         </View>
             
