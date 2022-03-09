73921eee11071f4f00ba65eaad3395bd256cbf1e
Jessica Rosalia Fernandes <jessicarosalia678@gmail.com>
2022-03-09 13:35:44 -0300

    Criando Componente Header

---
+++ front/src/components/Header/style.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    titleHeader: {
+        color: "#0C066B",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        fontWeight: "bold",
+        fontSize: 18,
+    },
+})
