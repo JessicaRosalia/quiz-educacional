e5894784d837665a841b398473b16edb112d025b
Jessica Rosalia Fernandes <jessicarosalia678@gmail.com>
2022-03-10 08:40:00 -0300

    Retirando código não usado do input e acrescentando propriedades no Input

--- front/src/components/InputText/index.js
+++ front/src/components/InputText/index.js
@@ -1,18 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
 import { Input } from 'react-native-elements/dist/input/Input';
 import Label from '../Label/index.js';
 import style from './style.js';
 
-const InputText = ({label, placeholder, onChangeValue}) => {
-
-    const [value, setValue] = useState("");
-    const [errorValue, setErrorValue] = useState(false);
+const InputText = ({label, placeholder, onChangeValue, errorMessage}) => {
 
     return (
         <View style={style.input}>
             <Label label={label} required={true}/>
-            <Input placeholder={placeholder} keyboardType="default" selectionColor={"#c3c3c3"} placeholderTextColor="#c3c3c3" onChangeText={value => onChangeValue(value)} errorMessage={errorValue} errorStyle={{ color: "red" }} style={{ color: "#000", fontSize: 15 }} />
+            <Input placeholder={placeholder} keyboardType="default" selectionColor={"#c3c3c3"} placeholderTextColor="#c3c3c3" onChangeText={value => onChangeValue(value)} errorMessage={errorMessage} errorStyle={{ color: "red" }} style={{ color: "#000", fontSize: 15 }} />
         </View>
     )
 }
